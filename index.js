29579fe5c8a6ab853cbb1322651fdb3c961630b8
LukasZvikas <lzvikas1@gmail.com>
2019-02-27 23:21:55 -0500

    added simple grapql schema with query and mutation

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("graphql-express");
-const { buildSchema} = require("graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser);
 
-app.use('/graphql', {
-    schema: buildSchema(`
-        
+app.use("/graphql", {
+  schema: buildSchema(`
+        rootQuery: {
+            events: [String!]!
+        }
+
+        rootMutation: {
+            getEvent(name: String) String
+        }
+
+        schema: {
+            query: RootQuery
+            mutation: RootMutation
+        }
     `),
-    rootValue: {}
-})
+  rootValue: {
+    events: () => {
+      return ["Walkin", "Reading", "Sleeping"];
+    },
+    getEvents: args => {
+      const name = args.name;
+      return name;
+    }
+  },
+  graphiql: true
+});
 
 const PORT = process.env.PORT || 5000;
 
